9da5de4ae3333f91d3e0feb2804172f70532e175
imtiazraqib <imtiazud@ualberta.ca>
2024-11-21 19:02:16 -0500

    fixed gallery image reference type

--- src/components/gallery/MasonryGrid.tsx
+++ src/components/gallery/MasonryGrid.tsx
@@ -1,37 +1,44 @@
 "use client";
 
-import Masonry from 'react-masonry-css';
+import Masonry from "react-masonry-css";
 import { SmartImage } from "@/once-ui/components";
 import styles from "./Gallery.module.scss";
-import { useTranslations } from 'next-intl';
-import { renderContent } from '@/app/resources';
+import { useTranslations } from "next-intl";
+import { renderContent } from "@/app/resources";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+import { Key } from "react";
 
 export default function MasonryGrid() {
-    const breakpointColumnsObj = {
-        default: 4,
-        1440: 3,
-        1024: 2,
-        560: 1
-    };
+  const breakpointColumnsObj = {
+    default: 4,
+    1440: 3,
+    1024: 2,
+    560: 1,
+  };
 
-    const t = useTranslations();
-    const { gallery } = renderContent(t);
+  const t = useTranslations();
+  const { gallery } = renderContent(t);
 
-    return (
-        <Masonry
-            breakpointCols={breakpointColumnsObj}
-            className={styles.masonryGrid}
-            columnClassName={styles.masonryGridColumn}>
-            {gallery.images.map((image, index) => (
-                <SmartImage
-                    key={index}
-                    radius="m"
-                    aspectRatio={image.orientation === "horizontal" ? "16 / 9" : "9 / 16"}
-                    src={image.src}
-                    alt={image.alt}
-                    className={styles.gridItem}
-                />
-            ))}
-        </Masonry>
-    );
+  return (
+    <Masonry
+      breakpointCols={breakpointColumnsObj}
+      className={styles.masonryGrid}
+      columnClassName={styles.masonryGridColumn}>
+      {gallery.images.map(
+        (
+          image: { orientation: string; src: (string | StaticImport) & string; alt: string },
+          index: Key | null | undefined
+        ) => (
+          <SmartImage
+            key={index}
+            radius="m"
+            aspectRatio={image.orientation === "horizontal" ? "16 / 9" : "9 / 16"}
+            src={image.src}
+            alt={image.alt}
+            className={styles.gridItem}
+          />
+        )
+      )}
+    </Masonry>
+  );
 }
